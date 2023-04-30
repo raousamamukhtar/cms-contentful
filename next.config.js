9ce6d94b09b68cffa51e3d1d48c4acaaccfdd824
rmusamamukhtar <RMUSAMAMUKHTAR@GMAIL.COM>
2023-04-30 09:55:34 +0500

    cms

--- next.config.js
+++ next.config.js
@@ -3,6 +3,20 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  env: {
+    CONTENTFUL_SPACE_ID: "cey52njm1fzs",
+    CONTENTFUL_ACCESS_KEY: "QvrCfOHnOtYajeGLlCjHsPrYBSB6jhSq0pRzCzaKF9Q"
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+        port: '',
+      },
+    ],
+  },
+
 }
 
 module.exports = nextConfig
